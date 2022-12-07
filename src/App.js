092fd6e33575e99bd9df8b9488987b40961f2f47
Md Irshad <md.irshad@indusnet.co.in>
2022-12-07 18:58:04 +0530

    userdetails file created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Dashboard from './Components/Dashboard';
-import LandingPage from './Components/LandingPage';
+import Dashboard from "./Components/Dashboard";
+import LandingPage from "./Components/LandingPage";
 import { Routes, Route } from "react-router-dom";
-import UserVerification from './Components/UserVerification';
+import UserVerification from "./Components/UserVerification";
+import UserDetails from "./Components/UserDetails";
 
 function App() {
   return (
     <div className="App">
-     <Routes>
-      <Route path='/' element={<Dashboard />} />
-      <Route path='/landingpage' element={<LandingPage />} />
-      <Route path='/userverification' element={<UserVerification />} />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/landingpage" element={<LandingPage />} />
+        <Route path="/userverification" element={<UserVerification />} />
+        <Route path="/userdetails" element={<UserDetails />} />
       </Routes>
     </div>
   );
